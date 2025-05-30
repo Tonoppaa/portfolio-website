f1b491257a75e6b2eb8ac921a7779fad158f60bc
Tonoppaa <tommi.kalliopuska@gmail.com>
2025-05-30 13:35:44 +0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,3 @@
+function toggleMobileMenu(){ /* Kun klikataan Mobile Menua eli "hampurilaisikonia", luokka UL:lle lisätään ja poistetaan */
+    document.getElementById("menu").classList.toggle("active");
+}
